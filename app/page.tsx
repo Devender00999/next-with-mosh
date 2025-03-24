4560384e4cf0f46de52ac0a8f32508f6142839ac
Devender Kumar <devender@rubick.ai>
2025-03-24 10:34:27 +0530

    Added Optimized image using Image component from next

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
 "use client";
 import Link from "next/link";
 import ProductCart from "./components/ProductCart";
+import Image from "next/image";
+import coffee from "@/public/image.jpg";
 
 export default function Home() {
    return (
-      <main className="">
-         <div>Hello World</div>
+      <main className="relative h-screen">
+         {/* <Image src={coffee} alt="Coffee"  /> */}
+         <Image
+            src="https://bit.ly/react-cover"
+            fill
+            alt="React Cover"
+            style={{ objectFit: "cover" }}
+            sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+            quality={10}
+            priority
+         />
+         {/* <div>Hello World</div>
          <Link href="/users">Users</Link>
-         <ProductCart />
+         <ProductCart /> */}
       </main>
    );
 }
